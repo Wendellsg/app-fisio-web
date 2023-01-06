2853209ddb93e5a87cb40c70b0d1df86f90c12d8
Wendell Guimarães <wendell.silva.guimaraes@gmail.com>
2023-01-06 09:10:52 -0300

    Fix: Same fixes

--- pages/exercises/[id].js
+++ pages/exercises/[id].js
@@ -1,31 +1,49 @@
-import { useRouter } from 'next/router';
-import styles from './Exercise.module.css';
-import { useEffect, useRef, useState } from 'react';
-import OwnPlayer from '../../src/components/OwnPlayer';
-export default function PacientePage(){
-    const $videoRef = useRef(null)
-    const router = useRouter()
-    const { id } = router.query
-     const goBack = () => router.push('/exercises')
+import { useRouter } from "next/router";
+import styles from "./Exercise.module.css";
+import { useEffect, useRef, useState } from "react";
+import OwnPlayer from "../../src/components/OwnPlayer";
+export default function PacientePage() {
+  const $videoRef = useRef(null);
+  const router = useRouter();
+  const { id } = router.query;
+  const goBack = () => router.push("/exercises");
 
-    return(
-        <div className={styles.ExerciseContainer}>
-            <div className={styles.PlayerWrapper}>
-                <video
-                className={styles.Player}
-                ref={$videoRef}
-                poster={'/assets/atictivityimage.png'}
-                src='https://vjs.zencdn.net/v/oceans.mp4'/> 
-                <OwnPlayer  $videoRef={$videoRef} goBack={goBack}/>
-            </div>
-            <main>
-                <h1>Resumo</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore...</p>
-                <h1>Instruções</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore...</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore...</p>
-            </main>
-           
-        </div>
-    )
+  return (
+    <div className={styles.ExerciseContainer}>
+      <div className={styles.PlayerWrapper}>
+        <video
+          className={styles.Player}
+          ref={$videoRef}
+          poster={"/assets/atictivityimage.png"}
+          src="https://vjs.zencdn.net/v/oceans.mp4"
+        />
+        <OwnPlayer $videoRef={$videoRef} goBack={goBack} exerciceId={id} />
+      </div>
+      <main className={styles.ExerciseContent}>
+        <h1>Resumo</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore...
+        </p>
+        <h1>Instruções</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore...
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore...
+        </p>
+      </main>
+    </div>
+  );
 }
--- src/components/OwnPlayer/index.js
+++ src/components/OwnPlayer/index.js
@@ -1,57 +1,77 @@
-import styles from './OwnPlayer.module.css'
-import {AiOutlinePlayCircle, AiOutlinePauseCircle} from 'react-icons/ai'
-import {IoMdArrowBack } from 'react-icons/io'
-import { useEffect, useState } from 'react';
-export default function OwnPlayer({$videoRef, goBack}){
-    const Player = $videoRef.current;
-    const [playing, setPlaying] = useState(false)
-    const [playerPressed, setPlayerPressed] = useState(true)
-    const [currentTime, setCurrentTime] = useState(0)
+import styles from "./OwnPlayer.module.css";
+import { AiOutlinePlayCircle, AiOutlinePauseCircle } from "react-icons/ai";
+import { IoMdArrowBack } from "react-icons/io";
+import { useEffect, useState } from "react";
+export default function OwnPlayer({ $videoRef, goBack, exerciceId }) {
+  const Player = $videoRef.current;
+  const playing = !Player?.paused;
+  const [playerPressed, setPlayerPressed] = useState(true);
+  const [currentTime, setCurrentTime] = useState(0);
 
-
-    useEffect(()=>{
-        if(playing){
-            setPlayerPressed(false)
-        }
-    },[playing])
-
-    useEffect(()=>{
-        setPlaying(false)
-        onTimeUpdate()
-    },[])
-
-    function onTimeUpdate(){
-        if(!Player) return;
-        Player.ontimeupdate= ()=>{
-            setCurrentTime(Player.currentTime)
-        }
+  useEffect(() => {
+    console.log("playing", playing);
+    if (playing) {
+      setPlayerPressed(false);
     }
+  }, [playing]);
 
-    const fixTime= (time) =>{
-        if(!time||isNaN(time)) return '00'
-        let fixedTime = Math.floor(time).toString()
-        return fixedTime
-    }
+  useEffect(() => {
+    console.log("component mont", playing);
+    onTimeUpdate();
 
-    return(
-        <div className={`${styles.OwnPlayerContainer} ${!playerPressed?'fadeOut':'show'}`} onClick={()=>[setPlayerPressed(!playerPressed)]}>
+    return () => {
+      Player?.pause();
+      console.log("component unmount", playing);
+    };
+  }, []);
 
-            <div style={{width: '100%', display: 'flex', }}>
-            <IoMdArrowBack size={50} color={'#96FFB3'} onClick={()=>goBack()}/>
-            </div>
+  function onTimeUpdate() {
+    if (!Player) return;
+    Player.ontimeupdate = () => {
+      setCurrentTime(Player.currentTime);
+    };
+  }
 
-            <div style={{width: '100%', display: 'flex', justifyContent: 'center'}}>
+  const fixTime = (time) => {
+    if (!time || isNaN(time)) return "00";
+    let fixedTime = Math.floor(time).toString();
+    return fixedTime;
+  };
 
-            {!playing?<AiOutlinePlayCircle size={50} color={'#96FFB3'} onClick={()=>[setPlaying(!playing), Player?.play()]}/>:
-            <AiOutlinePauseCircle size={50} color={'#96FFB3'} onClick={()=>[setPlaying(!playing), Player?.pause()]}/>
-            }
-           
+  return (
+    <div
+      className={`${styles.OwnPlayerContainer} ${
+        !playerPressed ? "fadeOut" : "show"
+      }`}
+      onClick={() => [setPlayerPressed(!playerPressed)]}
+    >
+      <div style={{ width: "100%", display: "flex" }}>
+        <IoMdArrowBack size={50} color={"#96FFB3"} onClick={() => goBack()} />
+      </div>
 
-            </div>
-            <div className={styles.OwnPlayerFooter}>
-                <h2 className={styles.OwnPlayerContainerTitle}>Agachamento bilateral</h2>
-                <span className={styles.OwnPlayerTimer}>{`${fixTime(currentTime)} / ${fixTime(Player?.duration)}`}</span>
-            </div>
-        </div>
-    )
+      <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+        {!playing ? (
+          <AiOutlinePlayCircle
+            size={50}
+            color={"#96FFB3"}
+            onClick={() => [Player?.play()]}
+          />
+        ) : (
+          <AiOutlinePauseCircle
+            size={50}
+            color={"#96FFB3"}
+            onClick={() => [Player?.pause()]}
+          />
+        )}
+      </div>
+      <div className={styles.OwnPlayerFooter}>
+        <h2 className={styles.OwnPlayerContainerTitle}>
+          Agachamento bilateral
+        </h2>
+        <span className={styles.OwnPlayerTimer}>{`${fixTime(
+          currentTime
+        )} / ${fixTime(Player?.duration)}`}</span>
+      </div>
+    </div>
+  );
 }
